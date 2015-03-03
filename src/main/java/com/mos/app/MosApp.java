ca5fe33d4f5f93f7278be2405509be9354cb737c
Peter Chei <peterchei@outlook.com>
2015-03-04 07:44:06 +0800

    commit.

--- src/main/java/com/mos/app/MosApp.java
+++ src/main/java/com/mos/app/MosApp.java
@@ -1,8 +1,28 @@
 package com.mos.app;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mos.messaging.IBrokerServer;
+
 public class MosApp {
 
 	public static void main(String args[]) {
 		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+
+		try {
+			IBrokerServer brokerServer = (IBrokerServer) context.getBean("embededBrokerServer");
+			brokerServer.start("tcp://localhost:8081");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // start broker service at given URL.
 	}
 }
