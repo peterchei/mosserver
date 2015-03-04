32edcb60ce07f30cdac99375e96cf4eaf3a25d69
Peter Chei <peterchei@outlook.com>
2015-03-04 22:04:04 +0800

    go go go

--- src/main/java/com/mos/app/MosApp.java
+++ src/main/java/com/mos/app/MosApp.java
@@ -1,16 +1,23 @@
 package com.mos.app;
 
+import org.slf4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 import com.mos.messaging.IBrokerServer;
 
 public class MosApp {
+	
+	private static Logger logger = getLogger(MosApp.class);
 
 	public static void main(String args[]) {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
+	  
+		
+		
 
 		try {
 			IBrokerServer brokerServer = (IBrokerServer) context.getBean("embededBrokerServer");
@@ -19,5 +26,11 @@ public class MosApp {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} // start broker service at given URL.
+		
+		logger.info("*****************************************");
+		logger.info("******   All services started.  *********");
+		logger.info("*****************************************");
+		
+		
 	}
 }
--- src/main/java/com/mos/event/EventPublisher.java
+++ src/main/java/com/mos/event/EventPublisher.java
@@ -2,8 +2,8 @@ package com.mos.event;
 
 public interface EventPublisher {
 	
-	public abstract void publish(Notification Notification);	
-	public abstract void start();
-	public abstract void stop();
+	public void publish(Notification Notification);	
+	public void start();
+	public void stop();
 	
 }
