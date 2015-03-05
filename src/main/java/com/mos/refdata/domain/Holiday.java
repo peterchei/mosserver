3e3c70dff6710a216cf6af4fdb93fac4e2bd50be
Peter Chei <peterchei@outlook.com>
2015-03-05 21:53:04 +0800

    go go go

--- src/main/java/com/mos/refdata/domain/Holiday.java
+++ src/main/java/com/mos/refdata/domain/Holiday.java
@@ -1,12 +1,15 @@
 package com.mos.refdata.domain;
 
 public class Holiday {
+	
+	
 
 	enum HolidayType {
 		Trade, Settlement
 	}
 
 	private int date;
+	private long marketId;
 
 	/*
 	 * Date in yyyyMMdd format
--- src/main/java/com/mos/refdata/domain/Market.java
+++ src/main/java/com/mos/refdata/domain/Market.java
@@ -1,5 +1,12 @@
 package com.mos.refdata.domain;
 
+
 public class Market {
+	
+	private long marketId;
+	private String name;
+	private String shortName;
+	
+	
 
 }
