0570503916e146b053c329be6fe0f2b453d66d5e
Peter Chei <peterchei@outlook.com>
2015-03-05 20:52:44 +0800

    go go go

--- src/main/java/com/mos/store/AbstractStore.java
+++ src/main/java/com/mos/store/AbstractStore.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+
 public class AbstractStore<T> {
 	
 	protected EntityManager entityManager =null;
