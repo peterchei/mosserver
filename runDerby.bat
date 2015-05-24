2fc516bca3b6541f497d1c1f66b12b64f912b1dd
Peter Chei <peterchei@outlook.com>
2015-05-24 23:10:43 +0800

    move from oracle to Derby Java DB.

---
+++ runDerby.bat
@@ -0,0 +1,2 @@
+cd d:\temp
+java -jar %DERBY_HOME%\lib\derbyrun.jar server start
---
+++ runDerby.sh
@@ -0,0 +1,3 @@
+DERBY_HOME=/cygdrive/d/DevTools/jdk1.8.0_25/db
+java -jar $DERBY_HOME/lib/derbyrun.jar server start
+
