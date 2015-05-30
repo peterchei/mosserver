71d76db33cc9ffa397c7facc4659747b53559398
peterchei <peterchei@outlook.com>
2015-05-30 23:26:11 +0800

    prototype

--- runDerby.sh
+++ runDerby.sh
@@ -1,3 +1,6 @@
-DERBY_HOME=/cygdrive/d/DevTools/jdk1.8.0_25/db
+
+mkdir db
+cd db
+DERBY_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/db
 java -jar $DERBY_HOME/lib/derbyrun.jar server start
 
