47c20f45b06a7d6fe32e4f700aa0d001e023a6f0
peter chei <peterchei@outlook.com>
2015-06-14 08:29:05 +0800

    commit

--- src/main/java/com/mos/refdata/domain/Book.java
+++ src/main/java/com/mos/refdata/domain/Book.java
@@ -1,5 +1,19 @@
 package com.mos.refdata.domain;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private long id;
+	private String name;
+	private String shortName;
+	private long legalEntityId;
+	
 }
