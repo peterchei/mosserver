54ad0f1aa2bd8f26c793736f007dd5e998cc72f7
Peter Chei <peterchei@outlook.com>
2019-09-11 22:35:55 +0800

    typotyping

--- runDerbyWin.sh
+++ runDerbyWin.sh
@@ -1,7 +1,8 @@
 
 mkdir db
 cd db
-DERBY_HOME=/c/DevTools/Java/jdk1.8.0_101/db
+JAVA_HOME=/C/DevTools/java/12.0.1
+PATH=/C/DevTools/java/12.0.1/bin
 DERBY_HOME=/C/DevTools/db-derby-10.15.1.3-bin
 java -jar $DERBY_HOME/lib/derbyrun.jar server start
 
